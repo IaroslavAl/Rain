ecc686bd2a399a06d254f1e1956b402c810a11bd
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-09-22 16:22:14 +0300

    Initial commit

---
+++ Rain/Rain.swift
@@ -0,0 +1,20 @@
+//
+//  Rain.swift
+//  Rain
+//
+//  Created by BELDIN Yaroslav on 22.09.2023.
+//
+
+import SwiftUI
+
+struct Rain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Rain_Previews: PreviewProvider {
+    static var previews: some View {
+        Rain()
+    }
+}
---
+++ Rain/RainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainApp.swift
+//  Rain
+//
+//  Created by Iaroslav Beldin on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
